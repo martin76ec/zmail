d8549d1e2c7d687faae9ec1ed447cd8b33db1e15
martin76ec <larreaamartin@gmail.com>
2023-04-26 21:20:35 -0500

    feat: add header menu

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Text from '@atoms/Text'
 import Container from '@atoms/Container'
 import NoteList from '@components/NoteList'
+import HeaderBar from '@components/HeaderBar'
+import FeatherIcon from '@components/Icon'
 
 const viewStyles = {
   flex: 1,
@@ -14,6 +15,10 @@ export default function MainScreen() {
   return (
     <Container justifyContent="center" alignItems="center">
       <NoteList />
+      <HeaderBar>
+        <FeatherIcon name="menu" size={22} />
+        <FeatherIcon name="more-vertical" size={22} />
+      </HeaderBar>
     </Container>
   )
 }
